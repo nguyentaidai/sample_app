11ba5dfc5aacffaab7c85463d080b56b6da6dcef
Dai Vit <nguyentaidaipt@gmail.com>
2019-01-27 21:26:47 +0700

    chapter_6

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    before_save :email.downcase
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
+    validates :email, presence: true,length: {maximum: Settings.max_length_email},
+                      format: {with: VALID_EMAIL_REGEX},
+                      uniqueness: {case_sensitive: false}
+    validates :name,  presence: true, length: { maximum: Settings.max_length_name}
+    validates :password, presence: true, length: { minimum: Settings.min_length_pass}
+  
+    has_secure_password
+  end
